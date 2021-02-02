0e39729231682a1861c9dc9cfcbeaff3bbfbb3a9
EC2 Default User <ec2-user@ip-172-31-26-205.ec2.internal>
2021-02-02 01:08:20 +0000

    画像を添付できるgem refileを追加

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -27,6 +27,6 @@ class TodolistsController < ApplicationController
     end
     private
     def list_params
-      params.require(:list).permit(:title, :body)
+      params.require(:list).permit(:title, :body, :image)
     end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '54c6a835c082bacf86980d30be42e4da43eeb002d31959b10f17760c4b0912bc3310825f4a31460d1323c2b67814dd7c611ca5f422bc16d88111c1b24a0af6d7'
